2c516442d3aaeecfc23ad351f83aa22f0d74978c
Nathalia-Urenha <naty.urenha@gmail.com>
2021-09-12 18:43:05 -0300

    Composer

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bd6304e16aa1658ad2266d852f94ab2688d14d46',
+    'name' => 'nathalia/composer',
+  ),
+  'versions' => 
+  array (
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+    ),
+    'nathalia/composer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bd6304e16aa1658ad2266d852f94ab2688d14d46',
+    ),
+  ),
+);
